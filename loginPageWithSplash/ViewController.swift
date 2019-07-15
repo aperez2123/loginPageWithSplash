7fea1fb7d7bdf546a32ce1d93802e6b19b29de78
Alexis <aperez50759@gamil.com>
2019-07-15 09:35:27 -0500

    Initial Commit

---
+++ loginPageWithSplash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loginPageWithSplash
+//
+//  Created by SER-YCL8 on 7/15/19.
+//  Copyright © 2019 SER-YCL8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
